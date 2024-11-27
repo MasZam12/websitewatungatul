ccc2550c32f800ab70d72a8c2f5c8ef116c719ef
MasZam12 <135273176+MasZam12@users.noreply.github.com>
2024-11-27 14:49:36 +0700

    a

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
-var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    spaceBetween: 30,
-    loop: true,
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    centeredSlides: true,
+var swiper = new Swiper(".swiper-container", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  centeredSlides: true,
 });
